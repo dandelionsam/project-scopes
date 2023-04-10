61c8e1e2d6388c64da8ef9df8e2a8e133c664169
Simone Aiello <aiellosimone.ct@gmail.com>
2023-04-10 17:30:29 +0300

    feat: argv variables check w/ minimist

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const fs = require('fs');
 const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
 
-const directoryPath = process.env.npm_config_directory || './';
-const generateMarkdown = process.env.npm_config_markdown || false;
+const directoryPath = argv['directory'] || process.env.npm_config_directory;
+const generateMarkdown = argv['markdown'] ? true : process.env.npm_config_markdown;
 
 function readDirectories(dirPath) {
   // Restituisce una promise che risolve con l'elenco delle cartelle di primo livello
@@ -12,8 +13,8 @@ function readDirectories(dirPath) {
     // Cerca la cartella "src"
     const srcPath = path.join(dirPath, 'src');
 
-    console.log('Cartella root del progetto: \t\t %c%s', 'color: #00a3cc', directoryPath);
-    console.log('Cartella per la ricerca: \t %c%s \n', 'color: #aa00ff', srcPath);
+    console.log('Cartella root del progetto: %c%s', 'color: #00a3cc', directoryPath);
+    console.log('Cartella per la ricerca: %c%s', 'color: #aa00ff', srcPath);
 
     fs.access(srcPath, fs.constants.F_OK, (err) => {
       if (err) {
@@ -41,9 +42,9 @@ function readDirectories(dirPath) {
   });
 }
 
-function generateMarkdownFile(path, directories) {
+function generateMarkdownFile(dir, directories) {
   const markdownContent = `# Elenco delle cartelle nella cartella "src"\n\n${directories.join('\n')}`;
-  const markdownFilePath = path.join(path, 'SCOPES.md');
+  const markdownFilePath = path.join(dir, 'SCOPES.md');
 
   fs.writeFile(markdownFilePath, markdownContent, (err) => {
     if (err) {
